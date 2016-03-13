7622ccb65f128c520a5154177251a625b8e198b8
heleneprojet <helene.mazars@isen-lille.fr>
2016-03-13 13:27:07 +0100

    testmail1.3

---
+++ contact.php
@@ -0,0 +1,12 @@
+<form action="contact-us-send.php" method="post">
+<ul>
+	<li><label for="name">Your Name</label> 
+	    <input id="name" name="name" type="text" /></li>
+	<li><label for="email">Your Email</label>
+	    <input id="email" name="email" type="text" /></li>
+	<li><label for="comment">Your Name</label>
+	    <textarea id="comment" name="comment"></textarea></li>
+	<li><input id="submit" name="submit" type="submit" 
+		value="Click to Send Comments" /></li>
+</ul>
+</form>
