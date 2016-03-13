69f2560fcc66ef22f8c856805ad2f9e727760d51
heleneprojet <helene.mazars@isen-lille.fr>
2016-03-13 15:52:35 +0100

    Mail_Integration1.0

--- contact-us-send.php
+++ contact-us-send.php
@@ -1,4 +1,23 @@
 <?php
 
-mail('helene.mazars@isen-lille.fr', 'Sample Contact Us', 'coucou3');
+// submitted to server through form
+
+// build the email message by using
+// info received by the HTML form
+$msg = 'Name: ' .$_POST['name'] ."\n"
+	'Email: ' .$_POST['email'] ."\n"
+	'Comment: ' ."\n" .$_POST['comment'];
+
+// send email using PHP's built in 
+// command, mail()
+mail('helene.mazars@isen-lille.fr', 
+	'Sample Comments', $msg);
+
+// redirect to the thank you page
+header('location: index.html');
+
+// exit this script - just to make sure nothing else gets run
+exit(0);
+
+
 ?>
