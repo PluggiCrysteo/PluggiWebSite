b502e946747db7087f90ddb67e9789e277f3af58
heleneprojet <helene.mazars@isen-lille.fr>
2016-03-13 13:35:16 +0100

    testmail2.1

--- contact-us-send.php
+++ contact-us-send.php
@@ -1,18 +1,4 @@
 <?php
 
-
-if(isset($_POST['submit'])) {
-  $msg='Name: ' . $_POST['name'] . "/n" 
-  .'Email: ' . $_POST['email'] . "/n" 
-  .'Comment: ' . $_POST['comment'];
-  mail('helene_mouton@hotmail.fr', 'Sample Contact Us', 'coucou');
-  header('location: index.html')
-}
-else {
-  // do other code block
-  header('Location: index.html');
-  exit(0);
-}
-
-
+mail('helene_mouton@hotmail.fr', 'Sample Contact Us', 'coucou');
 ?>
